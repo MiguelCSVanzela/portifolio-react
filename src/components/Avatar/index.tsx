ed19277003baf277af16403fae07283cfcbc6e18
Miguel CS Vanzela <miguelclaudiodasilvavanzela@gmail.com>
2023-07-06 15:13:10 -0300

    Estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,9 @@
+import { FotoPerfil } from "./styles"
+
+const Avatar = () => {
+  return(
+    <FotoPerfil src="https://github.com/MiguelCSVanzela.png" />
+  )
+}
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,13 +1,20 @@
+import Avatar from "../../components/Avatar"
+import Paragrafo from "../../components/Paragrafo"
 import Titulo from "../../components/Titulo"
+import { Descricao, BotaoTema, SidebarContainer } from "./styles"
 
-const Sidebar = () => {
-  return(
+const Sidebar = () => (
     <aside>
-      <img src="https://github.com/MiguelCSVanzela.png"/>
+      <SidebarContainer>
+      <Avatar />
       <Titulo fontSize={20}>Miguel Claudio</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>MiguelCSVanzela</Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>Engenheiro FrontEnd</Descricao>
+      <BotaoTema>Troca Tema</BotaoTema>
+      </SidebarContainer>
     </aside>
   )
-}
+
 
 export default Sidebar
 
